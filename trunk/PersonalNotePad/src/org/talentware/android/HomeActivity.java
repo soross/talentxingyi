ce3f4b89432f98f662ff7fcc6ec36d3bade237b4
ray.yi345@gmail.com <ray.yi345@gmail.com@c89c9911-4f5b-ba3e-4df0-8e27543cdb4d>
2012-12-09 09:37:48 +0000

    import PersonalNotePad

---
+++ trunk/PersonalNotePad/src/org/talentware/android/HomeActivity.java
@@ -0,0 +1,15 @@
+package org.talentware.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
