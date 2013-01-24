af56946e7069d42cbfd200a84bfca411c8043e02
ray.yi345 <ray.yi345@gmail.com>
2013-01-24 14:06:08 +0000

    添加工具类 Functions 和日志输出类 Logger

---
+++ CommonAndProj/src/org/talentware/android/util/Logger.java
@@ -0,0 +1,35 @@
+package org.talentware.android.util;
+
+import android.util.Log;
+
+public class Logger {
+
+	private static final String TAG = "TalentWare";
+
+	private static final boolean DEBUG = true;
+
+	public static void v(final String TAG, final String Content) {
+		if (DEBUG) {
+			Log.v(TAG, Content);
+		}
+	}
+
+	public static void i(final String TAG, final String Content) {
+		if (DEBUG) {
+			Log.i(TAG, Content);
+		}
+	}
+
+	public static void d(final String TAG, final String Content) {
+		if (DEBUG) {
+			Log.d(TAG, Content);
+		}
+	}
+
+	public static void e(final String TAG, final String Content) {
+		if (DEBUG) {
+			Log.e(TAG, Content);
+		}
+	}
+
+}
