e3aa53ea040fd6dfbe009e5537d6ca046d353790
ray.yi345@gmail.com <ray.yi345@gmail.com@c89c9911-4f5b-ba3e-4df0-8e27543cdb4d>
2013-03-02 14:45:41 +0000

    All 优化import 格式化代码

--- trunk/imo/src/com/imo/network/Observer/PacketsObserver.java
+++ trunk/imo/src/com/imo/network/Observer/PacketsObserver.java
@@ -2,20 +2,25 @@ package com.imo.network.Observer;
 
 import java.nio.ByteBuffer;
 
-
 public interface PacketsObserver {
-	/* TCP Packet arrived **/
+	/* TCP Packet arrived * */
 	public void NotifyPacketArrived(String aConnectionId, short command);
-	/* HTTP Packet arrived **/
+
+	/* HTTP Packet arrived * */
 	public void NotifyHttpPacketArrived(String aConnectionId, ByteBuffer aBuffer);
-	/* Packet progress **/
-	public void NotifyPacketProgress(String aConnectionId, short command,short aTotalLen,short aSendedLen);
-	/* Packet Timeout **/
-	public void NotifyPacketTimeOut(String aConnectionId,short aErrorCode);
-	/* Packet Failed **/
-	public void NotifyPacketFailed(String aConnectionId,short aErrorCode);
-	/* Can Accept TCP Packet **/
+
+	/* Packet progress * */
+	public void NotifyPacketProgress(String aConnectionId, short command, short aTotalLen, short aSendedLen);
+
+	/* Packet Timeout * */
+	public void NotifyPacketTimeOut(String aConnectionId, short aErrorCode);
+
+	/* Packet Failed * */
+	public void NotifyPacketFailed(String aConnectionId, short aErrorCode);
+
+	/* Can Accept TCP Packet * */
 	public boolean CanAcceptPacket(int command);
-	/* Can Accept Http Packet **/
+
+	/* Can Accept Http Packet * */
 	public boolean CanAcceptHttpPacket();
 }
