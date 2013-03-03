6a6b111c10afa89478c1636b58b65b5a66f2eab2
ray.yi345@gmail.com <ray.yi345@gmail.com@c89c9911-4f5b-ba3e-4df0-8e27543cdb4d>
2013-03-03 02:43:57 +0000

    All 优化import 格式化代码

--- trunk/imo/src/com/imo/module/config/NotificationSetActivity.java
+++ trunk/imo/src/com/imo/module/config/NotificationSetActivity.java
@@ -7,45 +7,37 @@ import android.content.Intent;
 import android.view.View;
 
 import com.imo.R;
-import com.imo.activity.AbsBaseActivity;
 import com.imo.activity.AbsBaseActivityNetListener;
 import com.imo.view.SettingItemView;
 import com.imo.view.SettingItemView.OnSettingItemClickListener;
-import com.imo.view.TitleBar;
 
 /**
  * Õ®÷™…Ë÷√
- * 
- * @author CaixiaoLong
- *
  */
-public class NotificationSetActivity extends AbsBaseActivityNetListener implements OnSettingItemClickListener{
-	
+public class NotificationSetActivity extends AbsBaseActivityNetListener implements OnSettingItemClickListener {
+
 	private SettingItemView contact_msg_push;
-	
+
 	private SettingItemView notice_receiver_scope;
-	
+
 	public static void launch(Context c) {
 		Intent intent = new Intent(c, NotificationSetActivity.class);
 		c.startActivity(intent);
 	}
-	
 
 	@Override
 	protected void installViews() {
-		
-		setContentView(R.layout.notificationset_activity);
-		
-		contact_msg_push =(SettingItemView) findViewById(R.id.contact_msg_push);
-		
-		notice_receiver_scope =(SettingItemView) findViewById(R.id.notice_receiver_scope);
-		
 
-		mTitleBar.initDefaultTitleBar(resources.getString(R.string.back), 
-				resources.getString(R.string.notice_setting));
-		
+		setContentView(R.layout.notificationset_activity);
+
+		contact_msg_push = (SettingItemView) findViewById(R.id.contact_msg_push);
+
+		notice_receiver_scope = (SettingItemView) findViewById(R.id.notice_receiver_scope);
+
+		mTitleBar.initDefaultTitleBar(resources.getString(R.string.back), resources.getString(R.string.notice_setting));
+
 		mTitleBar.setLeftBtnListene(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -67,28 +59,26 @@ public class NotificationSetActivity extends AbsBaseActivityNetListener implemen
 
 	}
 
-
 	@Override
 	public void onSettingItemClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
-		case R.id.contact_msg_push:
-			
-			break;
-		case R.id.notice_receiver_scope:
-			
-			break;
+			case R.id.contact_msg_push:
 
-		default:
-			break;
+				break;
+			case R.id.notice_receiver_scope:
+
+				break;
+
+			default:
+				break;
 		}
 	}
-
 
 	@Override
 	public void NotifyHttpPacketArrived(String aConnectionId, ByteBuffer aBuffer) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- trunk/imo/src/com/imo/module/config/SystemSetActivity.java
+++ trunk/imo/src/com/imo/module/config/SystemSetActivity.java
@@ -21,20 +21,17 @@ import com.imo.view.SettingItemView.OnSettingItemClickListener;
 
 /**
  * 系统设置
- * 
- * @author CaixiaoLong
- * 
  */
-public class SystemSetActivity extends AbsBaseActivityNetListener implements OnSettingItemClickListener,IAppUpdate {
+public class SystemSetActivity extends AbsBaseActivityNetListener implements OnSettingItemClickListener, IAppUpdate {
 
 	private SettingItemView[] mSettingItemViews = new SettingItemView[7];
 	public static final String IS_SHOCK = "is_shock";
 	public static final String IS_SOUND = "is_sound";
 	public static final String IS_NOTIFICATION = "is_notification";
 
-	private int[] item_ids = { R.id.notice_setting, R.id.sound_prompt,
-			R.id.vibrate_prompt, R.id.state_setting, R.id.new_features,
-			R.id.app_update, R.id.about_app };
+	private int[] item_ids = {
+			R.id.notice_setting, R.id.sound_prompt, R.id.vibrate_prompt, R.id.state_setting, R.id.new_features, R.id.app_update, R.id.about_app
+	};
 
 	public static void launch(Context c) {
 		Intent intent = new Intent(c, SystemSetActivity.class);
@@ -45,8 +42,7 @@ public class SystemSetActivity extends AbsBaseActivityNetListener implements OnS
 	protected void installViews() {
 		setContentView(R.layout.systemset_activity);
 
-		mTitleBar.initDefaultTitleBar(resources.getString(R.string.back),
-				resources.getString(R.string.system_setting));
+		mTitleBar.initDefaultTitleBar(resources.getString(R.string.back), resources.getString(R.string.system_setting));
 
 		mTitleBar.setLeftBtnListene(new View.OnClickListener() {
 
@@ -59,21 +55,21 @@ public class SystemSetActivity extends AbsBaseActivityNetListener implements OnS
 		for (int i = 0; i < mSettingItemViews.length; i++) {
 			mSettingItemViews[i] = (SettingItemView) findViewById(item_ids[i]);
 		}
-		
+
 		mSettingItemViews[0].initCheckedState(Globe.is_notification);
 		mSettingItemViews[1].initCheckedState(Globe.is_sound);
 		mSettingItemViews[2].initCheckedState(Globe.is_shock);
 
 	}
-	
+
 	@Override
 	public void showUpdateDialog(Message msg) {
 		if (msg.obj != null) {
 			UpdateManager update = new UpdateManager(mContext, (String) msg.obj);
 			update.showNoticeDialog();
-		}else{
+		} else {
 			hasRequested = false;
-			Toast.makeText(mContext, getString(R.string.app_no_update),Toast.LENGTH_LONG).show();
+			Toast.makeText(mContext, getString(R.string.app_no_update), Toast.LENGTH_LONG).show();
 		}
 	}
 
@@ -97,85 +93,74 @@ public class SystemSetActivity extends AbsBaseActivityNetListener implements OnS
 		Bundle bundle;
 
 		switch (v.getId()) {
-		case R.id.notice_setting:
-		{
-			String tip = null;
-			if (Globe.is_notification) {
-				tip = "已经取消通知提示";
-			} else {
-				tip = "已经设置通知提示";
+			case R.id.notice_setting: {
+				String tip = null;
+				if (Globe.is_notification) {
+					tip = "已经取消通知提示";
+				} else {
+					tip = "已经设置通知提示";
+				}
+				Globe.is_notification = !Globe.is_notification;
+				PreferenceManager.save(Globe.SP_FILE, new Object[] {
+						IS_NOTIFICATION, Globe.is_notification
+				});
+				Toast.makeText(this, tip, Toast.LENGTH_SHORT).show();
+				break;
 			}
-			Globe.is_notification = !Globe.is_notification;
-			PreferenceManager.save(Globe.SP_FILE, new Object[] { IS_NOTIFICATION,
-					Globe.is_notification });
-			Toast.makeText(this, tip, Toast.LENGTH_SHORT).show();
-			break;
-		}
-		case R.id.sound_prompt: {
-			String tip = null;
-			if (Globe.is_sound) {
-				tip = "已经取消声音提示";
-			} else {
-				tip = "已经设置声音提示";
+			case R.id.sound_prompt: {
+				String tip = null;
+				if (Globe.is_sound) {
+					tip = "已经取消声音提示";
+				} else {
+					tip = "已经设置声音提示";
+				}
+				Globe.is_sound = !Globe.is_sound;
+				PreferenceManager.save(Globe.SP_FILE, new Object[] {
+						IS_SOUND, Globe.is_sound
+				});
+				Toast.makeText(this, tip, Toast.LENGTH_SHORT).show();
+				break;
 			}
-			Globe.is_sound = !Globe.is_sound;
-			PreferenceManager.save(Globe.SP_FILE, new Object[] { IS_SOUND,
-					Globe.is_sound });
-			Toast.makeText(this, tip, Toast.LENGTH_SHORT).show();
-			break;
-		}
-		case R.id.vibrate_prompt:
-		{
-			String tip = null;
-			if (Globe.is_shock) {
-				tip = "已经取消震动提示";
-			} else {
-				tip = "已经设置震动提示";
+			case R.id.vibrate_prompt: {
+				String tip = null;
+				if (Globe.is_shock) {
+					tip = "已经取消震动提示";
+				} else {
+					tip = "已经设置震动提示";
+				}
+				Globe.is_shock = !Globe.is_shock;
+				PreferenceManager.save(Globe.SP_FILE, new Object[] {
+						IS_SHOCK, Globe.is_shock
+				});
+				Toast.makeText(this, tip, Toast.LENGTH_SHORT).show();
+				break;
 			}
-			Globe.is_shock = !Globe.is_shock;
-			PreferenceManager.save(Globe.SP_FILE, new Object[] { IS_SHOCK,
-					Globe.is_shock });
-			Toast.makeText(this, tip, Toast.LENGTH_SHORT).show();
-			break;
-		}
-		case R.id.state_setting:
+			case R.id.state_setting:
 
-			StateSetActivity.launch(mContext);
-			break;
-		case R.id.new_features:
-			bundle = new Bundle();
-			bundle.putBoolean("isFromSetting", true);
-			NewFeaturesActivity.launch(mContext, bundle);
-			break;
-		case R.id.app_update:
-			if (!hasRequested) {
-				hasRequested = !hasRequested;
-				doRequestAppVersion();
-			}
-			break;
-		case R.id.about_app:
-			AboutActivity.launch(mContext);
-			break;
+				StateSetActivity.launch(mContext);
+				break;
+			case R.id.new_features:
+				bundle = new Bundle();
+				bundle.putBoolean("isFromSetting", true);
+				NewFeaturesActivity.launch(mContext, bundle);
+				break;
+			case R.id.app_update:
+				if (!hasRequested) {
+					hasRequested = !hasRequested;
+					doRequestAppVersion();
+				}
+				break;
+			case R.id.about_app:
+				AboutActivity.launch(mContext);
+				break;
 
-		default:
-			break;
+			default:
+				break;
 		}
 	}
-	
+
 	public static boolean hasRequested = false;
 
-
-//	@Override
-//	public boolean CanAcceptPacket(int command) {
-//		
-//		super.CanAcceptPacket(command);
-//		
-//		if (command == IMOCommand.IMO_UPDATE_VERSION) {
-//			return true;
-//		}
-//		return false;
-//	}
-	
 	@Override
 	public void NotifyHttpPacketArrived(String aConnectionId, ByteBuffer aBuffer) {
 		// TODO Auto-generated method stub
