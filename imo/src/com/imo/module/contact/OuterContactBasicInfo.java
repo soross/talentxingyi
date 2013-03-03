9bc5b01153883cd8fa0fbc4e99ad7e1c0e16a486
ray.yi345 <ray.yi345@gmail.com>
2013-03-03 02:46:35 +0000

    All 优化import 格式化代码

--- imo/src/com/imo/module/contact/OuterContactBasicInfo.java
+++ imo/src/com/imo/module/contact/OuterContactBasicInfo.java
@@ -3,26 +3,22 @@ package com.imo.module.contact;
 import com.imo.util.LogFactory;
 
 /**
- * Outer Contact BasicInfo 
- * 
- * @author CaixiaoLong
- *
+ * Outer Contact BasicInfo
  */
 public class OuterContactBasicInfo {
-	
+
 	private int cid;
 	private int uid;
 	private String corpAccount;
 	private String userAccount;
 	private String name;
 	private int gender;
-	
+
 	public OuterContactBasicInfo() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	public OuterContactBasicInfo(int cid, int uid, String corpAccount,
-			String userAccount, String name, int gender) {
+
+	public OuterContactBasicInfo(int cid, int uid, String corpAccount, String userAccount, String name, int gender) {
 		this.cid = cid;
 		this.uid = uid;
 		this.corpAccount = corpAccount;
@@ -31,43 +27,53 @@ public class OuterContactBasicInfo {
 		this.gender = gender;
 		LogFactory.d("OuterBasicInfo", toString());
 	}
+
 	public int getCid() {
 		return cid;
 	}
+
 	public void setCid(int cid) {
 		this.cid = cid;
 	}
+
 	public int getUid() {
 		return uid;
 	}
+
 	public void setUid(int uid) {
 		this.uid = uid;
 	}
+
 	public String getCorpAccount() {
 		return corpAccount;
 	}
+
 	public void setCorpAccount(String corpAccount) {
 		this.corpAccount = corpAccount;
 	}
+
 	public String getUserAccount() {
 		return userAccount;
 	}
+
 	public void setUserAccount(String userAccount) {
 		this.userAccount = userAccount;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getGender() {
 		return gender;
 	}
+
 	public void setGender(int gender) {
 		this.gender = gender;
 	}
-	
-	
-	
+
 }
