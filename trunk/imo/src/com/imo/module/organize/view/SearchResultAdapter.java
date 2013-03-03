c56b30607745799289a5b0ac3ec4e856ab6d6000
ray.yi345@gmail.com <ray.yi345@gmail.com@c89c9911-4f5b-ba3e-4df0-8e27543cdb4d>
2013-03-03 04:37:31 +0000

    All 优化import 格式化代码

--- trunk/imo/src/com/imo/module/organize/view/SearchResultAdapter.java
+++ trunk/imo/src/com/imo/module/organize/view/SearchResultAdapter.java
@@ -16,187 +16,122 @@ import com.imo.util.LogFactory;
 
 /**
  * 搜索结果 适配器
- * 
- * @author CaixiaoLong
- *
  */
 public class SearchResultAdapter extends BaseAdapter {
-	
-	/**部门类型：*/
-	private int  TYPE_DEPTINFO = 1;
-	
-	/** 用户类型 */
-	private int  TYPE_USERINFO  = 2;
-	
+
 	private SearchResultAdapter adapter;
-	
+
 	private Context mContext;
-	
+
 	private ArrayList<Node> searchResultList = null;
-	
+
 	private int selectedPos = -1;
-	
-	public SearchResultAdapter( Context mContext) {
+
+	public SearchResultAdapter(Context mContext) {
 		adapter = this;
 		this.mContext = mContext;
 	}
-	
-	public SearchResultAdapter( Context mContext,ArrayList<Node> searchResultList) {
+
+	public SearchResultAdapter(Context mContext, ArrayList<Node> searchResultList) {
 		adapter = this;
 		this.mContext = mContext;
 		this.searchResultList = searchResultList;
 	}
-	
-	
-	public SearchResultAdapter getAdapter(){
+
+	public SearchResultAdapter getAdapter() {
 		return adapter;
 	}
-	
-	public void setSelectedPos(int pos){
+
+	public void setSelectedPos(int pos) {
 		this.selectedPos = pos;
 	}
-	
+
 	/**
 	 * 更新需要显示的结果信息
 	 * 
 	 * @param searchResultList
 	 */
-	public void setShowData(ArrayList<Node> searchResultList){
-		
+	public void setShowData(ArrayList<Node> searchResultList) {
+
 		this.searchResultList = searchResultList;
-		
-//		adapter.notifyDataSetChanged();
-		
+
+		// adapter.notifyDataSetChanged();
+
 		LogFactory.d("SearchResult", "searchResultList = " + searchResultList.size());
 	}
-	
-	
+
 	/**
 	 * 清空数据
 	 */
-	public void setEmpty(){
+	public void setEmpty() {
 		if (searchResultList != null) {
 			this.searchResultList.clear();
 			adapter.notifyDataSetChanged();
 		}
 	}
-	
-	
-	
+
 	private int MAX_COUNT = 10;
-	
+
 	@Override
 	public int getCount() {
-		if (searchResultList!= null) {
-			return searchResultList.size()>MAX_COUNT ? MAX_COUNT :searchResultList.size();
-		}else{
+		if (searchResultList != null) {
+			return searchResultList.size() > MAX_COUNT ? MAX_COUNT : searchResultList.size();
+		} else {
 			return 0;
 		}
-//		return searchResultList!= null ? searchResultList.size() : 0;
+		// return searchResultList!= null ? searchResultList.size() : 0;
 	}
 
 	@Override
 	public Node getItem(int position) {
 		return searchResultList.get(position);
 	}
-	
-//	/**
-//	 * 获得节点的类型 [初步的逻辑：从数据库查所有的信息 | 现在的逻辑:从数据库中只查Id]
-//	 * 
-//	 * @param position
-//	 * @return
-//	 */
-//	public int getType(int position){
-//		
-//		if (getItem(position) instanceof EmployeeInfoItem) {
-//			return TYPE_USERINFO;
-//		}else if (getItem(position) instanceof  DeptMaskItem) {
-//			return TYPE_DEPTINFO;
-//		}
-//		return 0;
-//	}
 
 	@Override
 	public long getItemId(int position) {
 		return position;
 	}
-	
-//	/**
-//	 * Get Object Id 
-//	 * 
-//	 * @param position
-//	 * @return
-//	 */
-//	public long getObjectId(int position) {
-//		
-//		if (getType(position) == TYPE_DEPTINFO) {
-//			((DeptMaskItem)getItem(position)).getDept_id();
-//		}else if (getType(position) == TYPE_USERINFO) {
-//			((EmployeeInfoItem)getItem(position)).getUid();
-//		}
-//		return 0;
-//	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		
-//		NodeView view = null;
-//		NodeData data = null;
-//		DeptMaskItem deptMaskItem = null;
-//		EmployeeInfoItem employeeInfoItem = null;
-//		if (convertView == null) {
-//			view = new NodeView(mContext,false); 
-//			convertView = view;
-//			convertView.setTag(view);
-//		}else{
-//			view = (NodeView)convertView.getTag();
-//		}
-////		if (getType(position) == TYPE_DEPTINFO) {
-////			deptMaskItem = ((DeptMaskItem)getItem(position));
-////			data = new NodeData(deptMaskItem.getName(), "()");
-////		}else if (getType(position) == TYPE_USERINFO) {
-////			employeeInfoItem = ((EmployeeInfoItem)getItem(position));
-////		}
-//		return view;
-
 		NodeView view = null;
 		Node node = null;
 		NodeData data = null;
-		
+
 		if (convertView == null) {
-			//	使用开关控制后少一部分逻辑操作
-			view = new NodeView(mContext,false); 
+			// 使用开关控制后少一部分逻辑操作
+			view = new NodeView(mContext, false);
 			convertView = view;
 			convertView.setTag(view);
-			
-//			LogFactory.d("new", "create a new Vie");
-		}else{
-			view = (NodeView)convertView.getTag();
+
+			// LogFactory.d("new", "create a new Vie");
+		} else {
+			view = (NodeView) convertView.getTag();
 		}
-		
+
 		node = searchResultList.get(position);
-		
+
 		if (node.getId() == EngineConst.uId) {
 			view.isMySelfNode = true;
-		}else{
+		} else {
 			view.isMySelfNode = false;
 		}
-		
+
 		if (node == null) {
 			LogFactory.d("SearchResult-getView", "Node == null postion: " + position);
 		}
-		
-		LogFactory.d("SearchResult-getView", "NodeName = " + node.getNodeData().nodeName  + "\t postion = " + position);
-		
+
+		LogFactory.d("SearchResult-getView", "NodeName = " + node.getNodeData().nodeName + "\t postion = " + position);
+
 		data = node.getNodeData();
-		
-		if(node != null){
-			if(!node.isDept()){
+
+		if (node != null) {
+			if (!node.isDept()) {
 				view.builderViewByType(NodeView.TYPE_LEAF_NODE);
-				view.setLNData(data,node.getOnLineState());
+				view.setLNData(data, node.getOnLineState());
 				view.setBackgroundResource(R.drawable.leaf_node_bg);
-				
-				//////TriangleClickListener  
+
+				// ////TriangleClickListener
 				Bundle dataBundle = null;
 				dataBundle = new Bundle();
 				dataBundle.putInt("cid", node.getCid());
@@ -204,24 +139,23 @@ public class SearchResultAdapter extends BaseAdapter {
 				dataBundle.putString("name", node.getNodeData().nodeName);
 				dataBundle.putBoolean("sex", node.getNodeData().isBoy);
 				view.setOnTriangleClickListener(dataBundle);
-				
-			}else{ 
+
+			} else {
 				view.builderViewByType(NodeView.TYPE_PARENT_NODE);
 				view.setPNData(data);
 				view.updateNodeState(node.getNodeState());
-//				view.setBackgroundResource(R.drawable.parent_node_bg);
+				// view.setBackgroundResource(R.drawable.parent_node_bg);
 				view.setBackgroundResource(R.drawable.imo_dept_gradual_bg);
 			}
-//			view.setPadding((node.getLevel())*20, 3,3, 3);
+			// view.setPadding((node.getLevel())*20, 3,3, 3);
 		}
-		
+
 		if (selectedPos == position) {
 			view.setBackgroundResource(R.drawable.leaf_selected);
 		}
-		
+
 		return view;
-	
+
 	}
-	
 
 }
--- trunk/imo/src/com/imo/module/organize/view/StructNavItemView.java
+++ trunk/imo/src/com/imo/module/organize/view/StructNavItemView.java
@@ -2,7 +2,6 @@ package com.imo.module.organize.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -12,40 +11,31 @@ import com.imo.R;
 
 /**
  * StructNavItemView
- * 
- * @author CaixiaoLong
- *
  */
 public class StructNavItemView extends LinearLayout {
-	
-	private Context mContext;
-	private LayoutInflater inflater;
-	private LinearLayout viewGroup;
-	private  String nodeName = null;
-	
-	private  TextView nextImg = null;
-	private  TextView parentNode = null;
-	
-	private OnClickListener listener;
+	private String nodeName = null;
+
+	private TextView nextImg = null;
+	private TextView parentNode = null;
 
 	public StructNavItemView(Context context) {
 		super(context);
 		init(context);
 	}
-	
+
 	public StructNavItemView(Context context, String nodeName) {
 		super(context);
 		this.nodeName = nodeName;
 		init(context);
 	}
-	
-	public TextView getParentNode(){
+
+	public TextView getParentNode() {
 		return parentNode;
 	}
-	
 
 	/**
 	 * 初始化方法
+	 * 
 	 * @param context
 	 * @param attrs
 	 */
@@ -53,76 +43,59 @@ public class StructNavItemView extends LinearLayout {
 		super(context, attrs);
 		init(context);
 	}
-	
-	public void setOnClickListener(final OnClickListener listener){
-		this.listener = listener;
-		
+
+	public void setOnClickListener(final OnClickListener listener) {
 		parentNode.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 			}
 		});
-		
+
 		parentNode.setOnTouchListener(new OnTouchListener() {
-			
+
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				
+
 				if (event.getAction() == MotionEvent.ACTION_UP) {
-					
+
 					listener.onClick(v);
 				}
-				
+
 				return false;
 			}
 		});
 	}
-	
-	
-    /**
-     * 更新显示状态：粗体
-     * 
-     * @param is
-     */
-    public void updateShow(boolean isBlod){
-    	parentNode.getPaint().setFakeBoldText(isBlod);
-    	parentNode.setText(parentNode.getText());
-    }
-	
-	
+
+	/**
+	 * 更新显示状态：粗体
+	 * 
+	 * @param is
+	 */
+	public void updateShow(boolean isBlod) {
+		parentNode.getPaint().setFakeBoldText(isBlod);
+		parentNode.setText(parentNode.getText());
+	}
+
 	private void init(Context context) {
-		this.mContext = context;
 		getViewByInflater(context);
 	}
 
 	private void getViewByInflater(Context mContext) {
-		inflater = LayoutInflater.from(mContext);
-		viewGroup = (LinearLayout) inflater.inflate(R.layout.struct_nav_item_view, this);
-		
 		parentNode = (TextView) findViewById(R.id.tv_parent);
 		if (null != nodeName) {
 			parentNode.setText(nodeName);
 		}
-		
+
 		nextImg = (TextView) findViewById(R.id.tv_next_img);
 	}
-	
-	public void hideNextImg(){
+
+	public void hideNextImg() {
 		nextImg.setVisibility(View.GONE);
 	}
-	
-	private void hideView(View view){
-		view.setVisibility(View.GONE);
-	}
-	
-	private void showView(View view){
-		view.setVisibility(View.VISIBLE);
-	}
-	
-	
-	public interface OnClickListener{
+
+	public interface OnClickListener {
 		public void onClick(View view);
 	}
-	
+
 }
