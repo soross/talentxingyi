b0463316723e8f7d58721beb2b2b5def7e54e926
ray.yi345@gmail.com <ray.yi345@gmail.com@c89c9911-4f5b-ba3e-4df0-8e27543cdb4d>
2013-05-21 14:26:40 +0000

    WeiboAuthActivity 添加微博绑定认证界面

---
+++ trunk/LocationBaseService/src/org/talentware/android/module/weibo/WeiboAuthActivity.java
@@ -0,0 +1,22 @@
+package org.talentware.android.module.weibo;
+
+import android.os.Bundle;
+import org.talentware.android.module.BaseActivity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yixing
+ * Date: 13-5-21
+ * Time: 下午9:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class WeiboAuthActivity extends BaseActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initViews() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
