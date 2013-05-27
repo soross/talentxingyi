f7fc3e12b0fbbce4e35121f4d4bccc1ae337e7f8
ray.yi345@gmail.com <ray.yi345@gmail.com@c89c9911-4f5b-ba3e-4df0-8e27543cdb4d>
2013-05-27 13:29:51 +0000

    添加必要注释

--- trunk/AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/future/Cancellable.java
+++ trunk/AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/future/Cancellable.java
@@ -1,7 +1,24 @@
 package com.koushikdutta.async.future;
 
 public interface Cancellable {
+
+    /**
+     * 任务是否已经完成
+     *
+     * @return true:任务已经完成,false:任务未完成
+     */
     boolean isDone();
+
+    /**
+     * 任务是否已经被取消
+     *
+     * @return true:任务已经被取消,false:任务未被取消
+     */
     boolean isCancelled();
+
+    /**
+     * 取消任务
+     * @return true:任务取消成功,false:任务取消失败
+     */
     boolean cancel();
 }
