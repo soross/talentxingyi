72bb8fd8a3e10cce266fd642b178d5e7f2083931
ray.yi345@gmail.com <ray.yi345@gmail.com@c89c9911-4f5b-ba3e-4df0-8e27543cdb4d>
2013-05-27 13:34:40 +0000

    添加必要注释

--- trunk/AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/future/DependentCancellable.java
+++ trunk/AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/future/DependentCancellable.java
@@ -1,6 +1,16 @@
 package com.koushikdutta.async.future;
 
 public interface DependentCancellable extends Cancellable {
+
+    /**
+     * 获取父Cancellable实例
+     *
+     * @return Cancellable实例
+     */
     public Cancellable getParent();
+
+    /**
+     * 设置父Cancellable实例
+     */
     public void setParent(Cancellable parent);
 }
