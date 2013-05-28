25619e49f16da59507a7020f4a90dbd27888e47c
ray.yi345 <ray.yi345@gmail.com>
2013-05-28 09:59:58 +0000

    RawHeaders 删除无用import

--- AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/callback/DataCallback.java
+++ AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/callback/DataCallback.java
@@ -3,7 +3,6 @@ package com.koushikdutta.async.callback;
 import com.koushikdutta.async.ByteBufferList;
 import com.koushikdutta.async.DataEmitter;
 
-
 public interface DataCallback {
     public void onDataAvailable(DataEmitter emitter, ByteBufferList bb);
 }
