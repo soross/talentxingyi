904c53b7db467857b3a8c8dbf14dee59f2e32404
ray.yi345@gmail.com <ray.yi345@gmail.com@c89c9911-4f5b-ba3e-4df0-8e27543cdb4d>
2013-06-08 14:32:50 +0000

    格式化代码

--- trunk/AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/future/SimpleCancelable.java
+++ trunk/AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/future/SimpleCancelable.java
@@ -4,34 +4,6 @@ public class SimpleCancelable implements DependentCancellable {
 
     boolean complete;
 
-    @Override
-    public boolean isDone() {
-        return complete;
-    }
-
-    public boolean setComplete() {
-        synchronized (this) {
-            if (canceled)
-                return false;
-            complete = true;
-        }
-        return true;
-    }
-
-    @Override
-    public boolean cancel() {
-        synchronized (this) {
-            if (complete)
-                return false;
-            if (canceled)
-                return true;
-            canceled = true;
-        }
-        if (parent != null)
-            parent.cancel();
-        return true;
-    }
-
     boolean canceled;
 
     Cancellable parent;
@@ -47,8 +19,38 @@ public class SimpleCancelable implements DependentCancellable {
     }
 
     @Override
+    public boolean isDone() {
+        return complete;
+    }
+
+    @Override
     public boolean isCancelled() {
+        // 逻辑：当前是否已经取消掉或者父类是否已经取消掉
         return canceled || (parent != null && parent.isCancelled());
+    }
+
+    @Override
+    public boolean cancel() {
+        // 当已经完成的时候，为false；已经cancel时候，为true。然后把父类的也cancel掉
+        synchronized (this) {
+            if (complete)
+                return false;
+            if (canceled)
+                return true;
+            canceled = true;
+        }
+        if (parent != null)
+            parent.cancel();
+        return true;
+    }
+
+    public boolean setComplete() {
+        synchronized (this) {
+            if (canceled)
+                return false;
+            complete = true;
+        }
+        return true;
     }
 
     public static final Cancellable COMPLETED = new SimpleCancelable() {
