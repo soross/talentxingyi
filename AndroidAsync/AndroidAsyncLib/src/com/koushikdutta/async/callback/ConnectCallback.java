732821a80e5d9540ef2e9e30d998791f5423bc2f
ray.yi345 <ray.yi345@gmail.com>
2013-06-08 13:08:09 +0000

    格式化代码

--- AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/callback/ConnectCallback.java
+++ AndroidAsync/AndroidAsyncLib/src/com/koushikdutta/async/callback/ConnectCallback.java
@@ -3,5 +3,12 @@ package com.koushikdutta.async.callback;
 import com.koushikdutta.async.AsyncSocket;
 
 public interface ConnectCallback {
+
+    /**
+     * 建立连接完毕
+     *
+     * @param ex     异常信息
+     * @param socket Socket实例
+     */
     public void onConnectCompleted(Exception ex, AsyncSocket socket);
 }
