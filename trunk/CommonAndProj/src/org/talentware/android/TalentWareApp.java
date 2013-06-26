6389575344c9ce78840dcdd3ed9fa39ed09bf7b2
ray.yi345@gmail.com <ray.yi345@gmail.com@c89c9911-4f5b-ba3e-4df0-8e27543cdb4d>
2013-06-26 02:52:20 +0000

    TalentWareApp 添加单例变量初始化赋值 ToastManager 添加Toast管理类 view_toast 添加toast布局类

--- trunk/CommonAndProj/src/org/talentware/android/TalentWareApp.java
+++ trunk/CommonAndProj/src/org/talentware/android/TalentWareApp.java
@@ -1,17 +1,16 @@
 package org.talentware.android;
 
-import org.talentware.android.broadcast.ConnectionChangeReceiver;
-
 import android.app.Application;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import org.talentware.android.broadcast.ConnectionChangeReceiver;
 
 public class TalentWareApp extends Application {
 
 	/** 监听网络状态改变类 */
 	private ConnectionChangeReceiver mConnectionChangeReceiver;
 
-	private TalentWareApp mInstance;
+	private static TalentWareApp mInstance;
 
 	public synchronized TalentWareApp getTalentWareApp() {
 		if (mInstance == null) {
@@ -21,12 +20,17 @@ public class TalentWareApp extends Application {
 	}
 
 	public void onCreate() {
+		mInstance = this;
 
 		// 注册监听网络状态改变类
 		mConnectionChangeReceiver = new ConnectionChangeReceiver();
 		IntentFilter mFilter = new IntentFilter();
 		mFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
 		registerReceiver(mConnectionChangeReceiver, mFilter);
+	}
+
+	public static TalentWareApp getAppContext() {
+		return mInstance;
 	}
 
 }
